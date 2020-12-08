12ebd38bd67a1041721d97a6b518d8ed7ee3d263
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-12-08 17:11:32 +0200

    app suitable for a mobile device

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     
     <!-- SEO Meta Tags -->
     <meta name="description" content="Aria is a business focused HTML landing page template built with Bootstrap to help you create lead generation websites for companies and their services.">
