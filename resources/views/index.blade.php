37fbb79203f4745305255dfcd26dc4102506674b
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-12-08 23:05:35 +0200

    adjusted home page for mobiles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
                             <!-- <h1>BUSINESS <span id="js-rotating">TEMPLATE, SERVICES, SOLUTIONS</span></h1> -->
                             <!-- <h1>Reliance Compliance Solutions</h1> -->
                             <!-- <p class="p-heading p-large">RCS is a top consultancy company specializing in business growth using online marketing and conversion optimization tactics</p> -->
-                            <a style="margin-top: 1px; position: absolute; top: 2; right: 0;" class="btn-solid-lg page-scroll" href="/contact">SCHEDULE A CONSULTATION TODAY</a>
+                            <a style="margin-top: 1px; position: absolute; top: 2; right: 0; font-size:1vw;" class="btn-solid-lg page-scroll" href="/contact">SCHEDULE A CONSULTATION TODAY</a>
                         <!-- </div> -->
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
@@ -27,7 +27,7 @@
                 <div class="col-lg-5">
                     <div class="text-container">
                         <p>Our team of Certified AML Specialists has over 10 years experience in the industry and is ready to assist you with all your compliance 		and AML needs. We have a passion for helping others and seeing them succeed.  </p>
-                        <a class="btn-solid-lg page-scroll" href="/about#team">MEET THE TEAM</a>
+                        <a class="btn-solid-lg page-scroll" style="font-size: 0.9vw;" href="/about#team">MEET THE TEAM</a>
                     </div> <!-- end of text-container -->
                 </div> <!-- end of col -->
                 <div class="col-lg-7">
