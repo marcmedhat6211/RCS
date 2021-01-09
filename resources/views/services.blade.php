6d7f34ce9437a278d4fb01381237eec490742e81
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-01-09 16:33:42 +0200

    animation on services cards

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,32 +1,195 @@
     @extends('layouts.app')
     @section('content')
+    <div id="services" class="cards-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h1 style="margin-right: 100px;">Services</h1>
+                    </div>
+                    <br>
+                    <br>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!-- card 1 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">AML/Compliance effectiveness review</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We conduct a thorough AML/Compliance effectiveness review to your program as mandated by FINTRAC every 2 years. We also provide solutions to any findings in our review for an additional fee. <br><br></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <!-- card 2 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Assist with banking partner audit</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We assist by prepping you for the banking audit process as well as helping you gather all the necessary reports/documents to be presented for the audit. We also assist with your banking partner inquires about your program.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 3 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">AML program development</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We build your AML program from scratch (policies, operation, third party technology platforms) or we can assist you in enhancing your current program to address specific issues.<br><br><br></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 4 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Compliance regime development</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We build your compliance regime from scratch or we can assist you in amending yours to be compliant with the current regulations at the time. <br><br></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 5 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Policy write up or review</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We draft up new policies for your company relating to compliance, AML, RBA, Sanctions and ABC or we can review yours and make amendments. <br><br> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 6 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Act as Compliance officer or CAMLO</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We assign one of our CAMS certified AML specialist to act as your Compliance officer or Chief AML officer depending on your needs.<br><span style="font-size: 12px"> **Terms & Conditions Apply**</span> </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 7 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Assist with regulatory exams (FINTRAC/AMF)</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We assist by prepping you for the exam interview as well as helping you gather all the necessary reports/documents to be presented for the exam.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 8 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Draft action plan to address exam findings</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We assist by drafting up an action plan to address any findings from your exam and advise on how it can be amended/implemented in your policies or program.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- card 9 rotating -->
+                    <div class="card">
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <h3 class="card-title vertical">Act as AMF Respondent</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <h6 class="vertical-body">We assign one of our team members in Quebec to act as your AMF respondent for communication and document filing purposes between your company and the AMF.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!-- Services -->
     <div id="services" class="cards-2">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="section-title"><h1>Services</h1></div>
+                    <div class="section-title">
+                        <h1>Services</h1>
+                    </div>
                     <br>
                 </div> <!-- end of col -->
             </div> <!-- end of row -->
             <div class="row">
                 <div class="col-lg-12">
-                    
+
                     <!-- Card5 -->
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">AML/Compliance effectiveness review</h3>
-                            <p>We conduct a thorough AML/Compliance effectiveness review to your program as mandated by FINTRAC every 2 years. We also provide solutions to any findings in our review for an additional fee. <br><br> </p>
+                            <h6>We conduct a thorough AML/Compliance effectiveness review to your program as mandated by FINTRAC every 2 years. We also provide solutions to any findings in our review for an additional fee. <br><br> </h6>
                         </div>
                     </div>
                     <!-- end of card -->
 
-                    
                     <!-- Card8 -->
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Assist with banking partner audit</h3>
-                            <p>We assist by prepping you for the banking audit process as well as helping you gather all the necessary reports/documents to be presented for the audit. We also assist with your banking partner inquires about your program.</p>
+                            <h6>We assist by prepping you for the banking audit process as well as helping you gather all the necessary reports/documents to be presented for the audit. We also assist with your banking partner inquires about your program.</h6>
                         </div>
                     </div>
                     <!-- end of card -->
@@ -35,26 +198,26 @@
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">AML program development</h3>
-                            <p>We build your AML program from scratch (policies, operation, third party technology platforms) or we can assist you in enhancing your current program to address specific issues.<br><br><br></p>
+                            <h6>We build your AML program from scratch (policies, operation, third party technology platforms) or we can assist you in enhancing your current program to address specific issues.<br><br><br></h6>
                         </div>
                     </div>
                     <!-- end of card -->
-                    
+
                     <!-- Card2 -->
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Compliance regime development</h3>
-                            <p>We build your compliance regime from scratch or we can assist you in amending yours to be compliant with the current regulations at the time. <br><br> </p>
+                            <h6>We build your compliance regime from scratch or we can assist you in amending yours to be compliant with the current regulations at the time. <br><br> </h6>
                         </div>
                     </div>
                     <!-- end of card -->
-                    
-                    
+
+
                     <!-- Card4 -->
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Policy write up or review</h3>
-                            <p>We draft up new policies for your company relating to compliance, AML, RBA, Sanctions and ABC or we can review yours and make amendments. <br><br> </p>
+                            <h6>We draft up new policies for your company relating to compliance, AML, RBA, Sanctions and ABC or we can review yours and make amendments. <br><br> </h6>
                         </div>
                     </div>
                     <!-- end of card -->
@@ -63,17 +226,17 @@
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Act as Compliance officer or CAMLO</h3>
-                            <p>We assign one of our CAMS certified AML specialist to act as your Compliance officer or Chief AML officer depending on your needs.<br><span style="font-size: 12px"> **Terms & Conditions Apply**</span> </p>
+                            <h6>We assign one of our CAMS certified AML specialist to act as your Compliance officer or Chief AML officer depending on your needs.<br><span style="font-size: 12px"> **Terms & Conditions Apply**</span> </h6>
                         </div>
                     </div>
                     <!-- end of card -->
-                    
+
 
                     <!-- Card6 -->
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Assist with regulatory exams (FINTRAC/AMF)</h3>
-                            <p>We assist by prepping you for the exam interview as well as helping you gather all the necessary reports/documents to be presented for the exam.</p>
+                            <h6>We assist by prepping you for the exam interview as well as helping you gather all the necessary reports/documents to be presented for the exam.</h6>
                         </div>
                     </div>
                     <!-- end of card -->
@@ -82,7 +245,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Draft action plan to address exam findings</h3>
-                            <p>We assist by drafting up an action plan to address any findings from your exam and advise on how it can be amended/implemented in your policies or program.</p>
+                            <h6>We assist by drafting up an action plan to address any findings from your exam and advise on how it can be amended/implemented in your policies or program.</h6>
                         </div>
                     </div>
                     <!-- end of card -->
@@ -92,7 +255,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h3 class="card-title">Act as AMF Respondent</h3>
-                            <p>We assign one of our team members in Quebec to act as your AMF respondent for communication and document filing purposes between your company and the AMF.</p>
+                            <h6>We assign one of our team members in Quebec to act as your AMF respondent for communication and document filing purposes between your company and the AMF.</h6>
                         </div>
                     </div>
                     <!-- end of card -->
